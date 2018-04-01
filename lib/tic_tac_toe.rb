6ef35e96ffd0095673c096624bea88320be8f4c1
Asheley McDonald <asheleymcdonald@gmail.com>
2018-04-01 20:44:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,117 +1,91 @@
-#first, I defined the new_board method to create and hold variables for empty squares on grid
-	def new_board
-	  @a1 = " "
-	  @a2 = " "
-	  @a3 = " "
-	  @b1 = " "
-	  @b2 = " "
-	  @b3 = " "
-	  @c1 = " "
-	  @c2 = " "
-	  @c3 = " "
-	end
-	 
-	#then, I defined the squares method to turn the empty squares into an array
-	def squares
-	  [@a1, @a2, @a3, @b1, @b2, @b3, @c1, @c2, @c3]
-	  
-	end
-	 
-	def win_combos 
-	  [[@a1, @a2, @a3],
-	  [@a1, @b2, @c3],
-	  [@a1, @b1, @c1],
-	  [@b1, @b2, @b3],
-	  [@c1, @c2, @c3],
-	  [@c1, @b2, @a3],
-	  [@a2, @b2, @c2],
-	  [@a3, @b3, @c3]]
-	  
-	end
-	 
- def display_board(board)
-   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-   puts "-----------"
-   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-   puts "-----------"
-   puts " #{board[6]} | #{board[7]} | #{board[8]} "
- end
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
-	 
-	#def method to check if user's choice of move is valid/available. this should be called only after the player has had 3 turns but I don't know how to do that. 
-	def check_validity square_availability
-	  if square_availability == " "
-	    true
-	  else
-	    puts "That space is taken. Get your own!"
-	  end
-	end
-	 
-	def user_turn
-	  user_choice_hash = {"a1" => @a1,
-	                      "a2" => @a2,
-	                      "a3" => @a3,
-	                      "b1" => @b1,
-	                      "b2" => @b2,
-	                      "b3" => @b3,
-	                      "c1" => @c1,
-	                      "c2" => @c2,
-	                      "c3" => @c3}
-	                    
-	 
-	  puts "Please choose a square:"
-	  user_choice = STDIN.gets.chomp.downcase
-	  user_choice_hash.each do |choice, square|
-	    if user_choice == choice 
-	      if check_validity(square)
-	        square.sub!(" ", "X")
-	        display_board
-	      end
-	    elsif user_choice == "q" or user_choice == "quit"
-	      exit
-	    end
-	  end
-	  #check_for_winner
-	end
-	 
-	def comp_turn
-	  squares.shuffle.each do |square|
-	    if check_validity(square)
-	      square.gsub!(" ", "O")
-	      break
-	    end
-	  end
-	  #check_for_winner
-	end
-	 
-	def check_for_winner 
-	  win_combos.each do |combos| 
-	    if combos[0] == "O" && combos[1] == "O" && combos[2] == "O"
-	      puts "Computer wins! Good luck next time."
-	      exit 
-	    elsif combos[0] == "X" && combos[1] == "X" && combos[2] == "X"
-	      puts "You win! Good job!"
-	      exit
-	    #else
-	      #puts "Tie! Enter q to quit game."
-	    end
-	  end
-	end
-	 
-	def start_game
-	  puts "Welcome to my Tic Tac Toe game!"
-	  puts "To place your X on the grid, enter the location (ex--a1, b3)."
-	  puts "To quit, enter q at any time"
-	end
-	 
-	def run_game
-	  start_game
-	  new_board
-	  while true
-	    display_board
-	    user_turn
-	    comp_turn
-	  end
-	end
-	 
-	run_game
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def position_taken?(board, index)
+  board[index]== "X" || board[index] == "O"
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+
+def move(board, index, player)
+  board[index] = player
+end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end
